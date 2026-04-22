72f5f2e71e860b8bb74a84c8248377ac1fe0ee13
fab90 <fab90ro@hotmail.com>
2026-04-22 16:30:22 -0600

    Limpiar repositorio y dejar unicamente BusuhApp

--- BusUH.Core/Models/SeatAllocation.cs
+++
@@ -1,9 +0,0 @@
-namespace BusUH.Core.Models;
-
-public sealed class SeatAllocation
-{
-    public int RowNumber { get; init; }
-    public List<int> Seats { get; } = [];
-
-    public string SeatsFrame => string.Concat(Seats.Select(seat => seat.ToString())).PadRight(5, '0');
-}
--- BusUH.Core/Models/TerminalInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace BusUH.Core.Models;
-
-public sealed class TerminalInfo
-{
-    public string Code { get; set; } = string.Empty;
-    public string Name { get; set; } = string.Empty;
-
-    public override string ToString() => $"{Code} - {Name}";
-}
